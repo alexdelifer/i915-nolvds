8025736679ce4c9ee61924339ff63f7bc0e703f0
alex <alexdelifer@gmail.com>
2018-10-21 17:36:38 -0400

    change shit

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # i915-nolvds
 for use with x220 fhd mod, aka x320
 
-example:
+It's really simple, instead of rebuilding your kernel every time there's an update, or being locked in to using just 1 kernel, now you can just rebuild this i915 against your own kernel headers.
+
 ```
 i915-nolvds="/home/somepath/i915-nolvds/gpu/drm/i915"
 cd /usr/lib/modules/$(uname -r)/build/
---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#save run path as module root
+i915nolvds="$(pwd)"
+
+#cd to kernel headers on arch linux
+cd /usr/lib/modules/$(uname -r)/kernel/drivers/gpu/drm/i915
+
+if [ ! -f i915.ko.xz.lvds.bak ]; then
+        echo "Backup not Found!"
+        mv i915.ko.xz i915.ko.xz.lvds.bak
+        echo "i915.ko.xz backup to to i915.ko.xz.lvds.bak"
+fi
+rm i915.ko.xz
+cp ${i915nolvds}/gpu/drm/i915/i915.ko ./i915.ko
+xz -z ./i915.ko
+
+echo "Done!"
+
+#echo "make M=${i915nolvds}/gpu/drm/i915"
+#sudo make M="${i915nolvds}/gpu/drm/i915"
+
+# make sure i915 isn't loadded, probably blacklist it...
+
+#insmod $i915-nolvds/i915.ko
